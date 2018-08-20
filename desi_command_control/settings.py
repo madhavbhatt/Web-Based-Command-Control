752b968c04d08bc8c085ff6e440b7e03616595dd
madhavbhatt <madhav.bhatt73@gmail.com>
2018-08-19 23:09:32 -0400

    apache integration

--- desi_command_control/settings.py
+++ desi_command_control/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
     'sslserver',
     # 'django_mfa',
 ]
-a
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
